dc447a229c45485f14fcb1a6a9ea80434e5ece7e
ArkarMinKhant <93575427+ArkarMinKhant@users.noreply.github.com>
2021-11-02 16:08:30 +0630

    Add files via upload

---
+++ bonusjs.js
@@ -0,0 +1,59 @@
+let btnfirst1 = document.querySelector('#first1');
+let btnfirst2 = document.querySelector('#first2');
+let btnfirst3 = document.querySelector('#first3');
+let btnfirst4 = document.querySelector('#first4');
+let firstImage = document.querySelector('#image1');
+let firstVideo = document.querySelector('#video1')
+
+let btnsecond1 = document.querySelector('#second1');
+let btnsecond2 = document.querySelector('#second2');
+let btnsecond3 = document.querySelector('#second3');
+let btnsecond4 = document.querySelector('#second4');
+let secondImage = document.querySelector('#image2');
+let secondVideo = document.querySelector('#video2')
+
+let btnthird1 = document.querySelector('#third1');
+let btnthird2 = document.querySelector('#third2');
+let btnthird3 = document.querySelector('#third3');
+let btnthird4 = document.querySelector('#third4');
+let thirdImage = document.querySelector('#image3');
+let thirdVideo = document.querySelector('#video3')
+
+btnfirst1.addEventListener('click', () => btnfirst1.style.backgroundColor = 'red')
+btnfirst1.addEventListener('click', () => firstImage.style.opacity= 0)
+btnfirst1.addEventListener('click', () => firstVideo.style.opacity=1)
+btnfirst2.addEventListener('click', () => btnfirst2.style.backgroundColor = 'red')
+btnfirst2.addEventListener('click', () => firstImage.style.opacity= 0)
+btnfirst2.addEventListener('click', () => firstVideo.style.opacity=1)
+btnfirst3.addEventListener('click', () => btnfirst3.style.backgroundColor = 'green')
+btnfirst3.addEventListener('click', () => firstImage.style.opacity= 0)
+btnfirst3.addEventListener('click', () => firstVideo.style.opacity=1)
+btnfirst4.addEventListener('click', () => btnfirst4.style.backgroundColor = 'red')
+btnfirst4.addEventListener('click', () => firstImage.style.opacity= 0)
+btnfirst4.addEventListener('click', () => firstVideo.style.opacity=1)
+
+btnsecond1.addEventListener('click', () => btnsecond1.style.backgroundColor = 'red')
+btnsecond1.addEventListener('click', () => secondImage.style.opacity= 0)
+btnsecond1.addEventListener('click', () => secondVideo.style.opacity=1)
+btnsecond2.addEventListener('click', () => btnsecond2.style.backgroundColor = 'green')
+btnsecond2.addEventListener('click', () => secondImage.style.opacity= 0)
+btnsecond2.addEventListener('click', () => secondVideo.style.opacity=1)
+btnsecond3.addEventListener('click', () => btnsecond3.style.backgroundColor = 'red')
+btnsecond3.addEventListener('click', () => secondImage.style.opacity= 0)
+btnsecond3.addEventListener('click', () => secondVideo.style.opacity=1)
+btnsecond4.addEventListener('click', () => btnsecond4.style.backgroundColor = 'red')
+btnsecond4.addEventListener('click', () => secondImage.style.opacity= 0)
+btnsecond4.addEventListener('click', () => secondVideo.style.opacity=1)
+
+btnthird1.addEventListener('click', () => btnthird1.style.backgroundColor = 'red')
+btnthird1.addEventListener('click', () => thirdImage.style.opacity= 0)
+btnthird1.addEventListener('click', () => thirdVideo.style.opacity=1)
+btnthird2.addEventListener('click', () => btnthird2.style.backgroundColor = 'red')
+btnthird2.addEventListener('click', () => thirdImage.style.opacity= 0)
+btnthird2.addEventListener('click', () => thirdVideo.style.opacity=1)
+btnthird3.addEventListener('click', () => btnthird3.style.backgroundColor = 'green')
+btnthird3.addEventListener('click', () => thirdImage.style.opacity= 0)
+btnthird3.addEventListener('click', () => thirdVideo.style.opacity=1)
+btnthird4.addEventListener('click', () => btnthird4.style.backgroundColor = 'red')
+btnthird4.addEventListener('click', () => thirdImage.style.opacity= 0)
+btnthird4.addEventListener('click', () => thirdVideo.style.opacity=1)
---
+++ questions.js
@@ -0,0 +1,95 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many robotic legs does urgot have?",
+    answer: "Six",
+    options: [
+      "Four",
+      "Eight",
+      "Five",
+      "Six"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The colour of Teemo's glasses?",
+    answer: "Red",
+    options: [
+      "Brown",
+      "Red",
+      "Orange",
+      "Green"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The name of Warwick level.3 (E)?",
+    answer: "Primal Howl",
+    options: [
+      "Primal Howl",
+      "Blood Hunt",
+      "Infinite Duress",
+      "Jaws of the Beast"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the most expensive among the four?",
+    answer: "Cassiopeia",
+    options: [
+      "Karthus",
+      "Gangplank",
+      "Anivia",
+      "Cassiopeia"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which card of Twisted Fate stuns the enemy?",
+    answer: "Yellow",
+    options: [
+      "Blue",
+      "Green",
+      "Yellow",
+      "Red"
+    ]
+  },
+    {
+    numb: 6,
+    question: "How much is Sterak's Gage?",
+    answer: "3100",
+    options: [
+      "3200",
+      "3100",
+      "3000",
+      "2900"
+    ]
+  },
+    {
+    numb: 7,
+    question: "How many bushes in total are there in a normal map?",
+    answer: "44",
+    options: [
+      "48",
+      "36",
+      "44",
+      "40"
+    ]
+  },
+  ]
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+;
